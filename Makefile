d38cb6778a4f69f5d8fb5e6117f9c93bfdca0280
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-05-01 16:37:14 +0530

    structured register than absolute macros directly, this will ease the futhure task

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ OC:=arm-none-eabi-objcopy
 
 ARCH_FLAGS:=-mthumb -mcpu=cortex-m3
 STARTUP_DEFS=-D__STARTUP_CLEAR_BSS -D__START=main
-CFLAGS:=$(ARCH_FLAGS) $(STARTUP_DEFS) -Os -flto -ffunction-sections -fdata-sections
+CFLAGS:=$(ARCH_FLAGS) $(STARTUP_DEFS) -Os -flto -ffunction-sections -fdata-sections -I.
 
 USE_NOHOST:=--specs=nosys.specs
 GC:=-Wl,--gc-sections
--- main.c
+++ main.c
@@ -2,15 +2,8 @@
 	A simple blink program built for stm32f103 on a blue pill board or alike
 */
 
-#include<stdint.h>
-
-#define RCC_BASE 0x40021000
-#define RCC_APB2ENR (*((volatile uint32_t*)(RCC_BASE + 0x18)))
-
-#define GPIOC_BASE 0x40011000
-
-#define GPIOC_CRH (*((volatile uint32_t*)(GPIOC_BASE + 0x04)))
-#define GPIOC_ODR (*((volatile uint32_t*)(GPIOC_BASE + 0x0c)))
+#include<regs/rcc.h>
+#include<regs/gpio.h>
 
 void delay_for(volatile int clocks)
 {
@@ -22,14 +15,14 @@ void delay_for(volatile int clocks)
 
 void main(void)
 {
-	RCC_APB2ENR |= (1<<4);
-	GPIOC_CRH   &= 0xFF0FFFFF;
-	GPIOC_CRH   |= 0x00200000;
+	RCC->RCC_APB2ENR |= (1<<4);
+
+	GPIOC->GPIO_CRH   &= 0xFF0FFFFF;
+	GPIOC->GPIO_CRH   |= 0x00200000;
+
 	while(1)
 	{
-		GPIOC_ODR |= (1 << 13);
-		delay_for(1000000);
-		GPIOC_ODR &= (~(1 << 13));
+		GPIOC->GPIO_ODR ^= (1 << 13);
 		delay_for(1000000);
 	}
 }
